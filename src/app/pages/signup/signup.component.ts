2a477b4b6a18495457cf4f891694180fc760161a
Diego R <diego.riveros.g@mail.pucv.cl>
2023-11-12 14:32:57 -0300

    Actualización formulario registro

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { TdserviceService } from 'src/app/services/tdservice.service';
 
 @Component({
@@ -19,31 +19,73 @@ export class SignupComponent {
               {
 
     this.registro = this.fb.group({
-      nombre: ['', Validators.required],
-      apellido: ['', Validators.required],
-      email: ['', Validators.required],
-      contrasena: ['', Validators.required],
-      fecha_nacimiento: ['', Validators.required],
+      nombre: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(15)]),
+      apellido: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      contrasena: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
+      contrasenaRep: new FormControl({value: '', disabled: true}, [Validators.required, Validators.minLength(3), Validators.maxLength(15),this.confirmPasswordValidator.bind(this)]),
+      fecha_nacimiento: new FormControl('', [Validators.required])
     });
   }
 
   ngOnInit(): void {
-
+    this.registro.get('contrasena')?.valueChanges.subscribe(
+      (valorCampo) => {
+        if (this.registro.get('contrasena')?.valid) {
+          this.registro.get('contrasenaRep')?.enable();
+        } else {
+          this.registro.get('contrasenaRep')?.disable();
+        }
+  
+        if (valorCampo !== this.registro.get('contrasenaRep')?.value) {
+          this.registro.setErrors({ 'passwordMismatch': true });
+        } else {
+          this.registro.setErrors(null);
+        }
+      });
   }
+  
+  
+
+  getAge(fechaNacimiento: any): number {
+    const today = new Date();
+    const birthDate = new Date(fechaNacimiento);
+    const age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      return age - 1;
+    }
+    return age;
+  }
+  
+
+  confirmPasswordValidator(control: FormControl): { [key: string]: any } | null {
+    const password = this.registro?.get('contrasena')?.value;
+    const contraseñaRep = control.value;
+  
+    return password === contraseñaRep ? null : { 'passwordMismatch': true };
+  }
+  
+
 
   
   registrar() {
-    const datosRegistro = this.registro.value;
+    if (this.registro.valid) {
+      const datosRegistro = this.registro.value;
   
-    this.td_service.signUp(datosRegistro).subscribe(
-      (response) => {
-        console.log('Usuario registrado con éxito', response);
-        this.router.navigate(['/login']);
-      },
-      (error) => {
-        console.error('Error al registrar el usuario', error);
-        alert("Error de registro, verifica credenciales");
-      }
-    );
+      this.td_service.signUp(datosRegistro).subscribe(
+        (response) => {
+          console.log('Usuario registrado con éxito', response);
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Error al registrar el usuario', error);
+          alert("Error de registro, verifica credenciales");
+        }
+      );
+    } else {
+      console.log('Formulario inválido. Por favor, verifica los campos.');
+    }
   }
+  
 }
