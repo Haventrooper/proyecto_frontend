316693cbf12a979b26f15450a91d6f4363055c49
Diego R <diego.riveros.g@mail.pucv.cl>
2023-11-13 15:15:17 -0300

    Actualización general

--- src/app/pages/categorias/categorias.component.ts
+++ src/app/pages/categorias/categorias.component.ts
@@ -9,10 +9,12 @@ import { TdserviceService } from 'src/app/services/tdservice.service';
 })
 export class CategoriasComponent {
   idCategoria: any
-  actividades: any;
+  selectedCategoria: any;  // Almacena la categoría seleccionada
+  actividades: any[] = [];
   selectedActividad: any;
   displayModal: boolean = false;
   pasos: any;
+  categorias: any[] = [];
 
 
   constructor(private route: ActivatedRoute,
@@ -22,6 +24,7 @@ export class CategoriasComponent {
     this.route.params.subscribe((params) => {
       this.idCategoria = +params['idCategoria']; // Convierte el ID a número
       this.obtenerActividadesPorCat(this.idCategoria)
+      this.obtenerCategorias();
       // Ahora puedes utilizar this.idCategoria para cargar las actividades relacionadas con esta categoría
     });
   }
@@ -47,6 +50,30 @@ export class CategoriasComponent {
     this.displayModal = true;
   } 
 
+  seleccionarCategoria(categoria: any) {
+    const token = localStorage.getItem('token');
+
+    this.selectedCategoria = categoria;
+    if (token) {
+      this.td_service.getActividadesPorCategoria(categoria.id_categoria, token).subscribe(
+        (response: any) => {
+          this.actividades = response;
+        },
+        (error) => {
+          console.error('Error al obtener actividades:', error);
+        }
+      );
+    }
+    else{
+      console.error('Token no encontrado en el Local Storage');
+      }
+    }
+  
+    // Método para cerrar el diálogo
+    cerrarModal() {
+      this.displayModal = false;
+    }
+
   obtenerPasos(idActividad: number){
     const token = localStorage.getItem('token');
 
@@ -63,5 +90,46 @@ export class CategoriasComponent {
   }
   mostrarPasos(idActividad: number) {
     this.obtenerPasos(idActividad);
+  }
+
+  obtenerCategorias(){
+    const token = localStorage.getItem('token');
+
+    // Verifica que haya un token antes de hacer la llamada al servicio
+    if (token) {
+      this.td_service.getCategorias(token).subscribe(
+        (response: any) => {
+          // Maneja la respuesta exitosa, por ejemplo, asignando las categorías a tu variable
+          this.categorias = response;
+        },
+        (error) => {
+          // Maneja el error, por ejemplo, mostrando un mensaje al usuario
+          console.error('Error al obtener categorías:', error);
+        }
+      );
+    } else {
+      // Maneja el caso en que no haya un token disponible
+      console.error('Token no disponible. El usuario no está autenticado.');
+    }
+  } 
+  mostrarTodasLasActividades() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      this.selectedCategoria = null; // Reinicia la categoría seleccionada
+      this.td_service.getActividades(token).subscribe(
+        (response: any) => {
+          this.actividades = response;
+        },
+        (error) => {
+          console.error('Error al obtener todas las actividades:', error);
+        }
+      );
+    }else{
+      console.error('Token no disponible. El usuario no está autenticado.');
+
+    }
+    // Lógica para obtener todas las actividades
+    
   }
 }
